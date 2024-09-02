7147808e981a872e373512a565f02af6c7b6a610
DaleLore <DaleLoreNY@gmail.com>
2024-09-01 22:19:19 -0500

    DAY 01: Introduction to Python

---
+++ 01_Day_Introduction/helloworld.py
@@ -0,0 +1,22 @@
+# Day 1 - 30DaysOfPython Challenge
+## To run this file in terminal, python3 hellowworld.py
+
+## Operations
+print(3 + 4)             # addition(+)
+print(3 - 4)             # subtraction(-)
+print(3 * 4)             # multiplication(*)
+print(3 / 4)             # division(/)
+print(3 ** 4)            # exponential(**)
+print(3 % 4)             # modulus(%)
+print(3 // 4)            # Floor division operator(//)
+
+## Write strings (in the python interactive shell)
+print("Hello World")
+
+## Checking data types
+print(type(10))                                 # Int
+print(type(9.8))                                # Float
+print(type(3.14))                               # Float
+print(type(4-4j))                               # Complex
+print(type(['Asabeneh', 'Python', 'Finland']))  # List
+print(type({'name':'Lore'}))                    # Dictionary
