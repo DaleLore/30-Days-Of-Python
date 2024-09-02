420b5db8c1878b766f5fd00d9c551db513012c42
DaleLore <DaleLoreNY@gmail.com>
2024-09-01 22:41:24 -0500

    Day 01 Exercises

---
+++ 01_Day_Introduction/01_Day_introduction.md
@@ -0,0 +1,17 @@
+# Exercises - Day 1
+
+## Exercise: Level 1
+- Python operations
+- Print string
+- Python Data Types
+
+## Exercise: Level 2
+- Create a folder named day_1 inside 30DaysOfPython folder
+
+## Exercise: Level 3
+- Write an example for different Python data types
+- Find an Euclidian distance between (2, 3) and (10, 8)
+    - The [Euclidean distance](https://www.w3resource.com/python-exercises/python-basic-exercise-40.php) formula is used to find the distance between two points on an Euclidean plane
+    - Points p1=[x1,y1]
+    - Points p2=[x2,y2]
+    - Formula: d = √(x2-x1)² + (y2 - y1)
--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -20,3 +20,17 @@ print(type(3.14))                               # Float
 print(type(4-4j))                               # Complex
 print(type(['Asabeneh', 'Python', 'Finland']))  # List
 print(type({'name':'Lore'}))                    # Dictionary
+
+##  Euclidian distance
+### Need math module to use the square root function
+import math
+
+### Define the coordinates of the first point (p1) as a list.
+p1 = [2, 3]
+
+### Define the coordinates of the second point (p2) as a list.
+p2 = [10, 8]
+
+### The formula computes the Euclidean distance in a 2D space.
+distance = math.sqrt((10-2)**2 + (8-3)**2)
+print(distance)
