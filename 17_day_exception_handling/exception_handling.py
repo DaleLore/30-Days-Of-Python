ac66b9bf7827456400fa6c9d5e82fdcc90d9c5c7
DaleLore <DaleLoreNY@gmail.com>
2024-09-17 21:21:19 -0500

    Day 17 Exercises completed

--- 17_day_exception_handling/exception_handling.py
+++ 17_day_exception_handling/exception_handling.py
@@ -1,2 +1,10 @@
-# 1. Unpack the first five countries and store them in a variable nordic_countries, store Estonia and Russia in es, and ru respectively.
+# 1. Unpack the first five countries and store them in a variable nordic_countries
+# store Estonia and Russia in es, and ru respectively.
 names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+
+## Unpacking
+*nordic_countries, es, ru = names
+
+print("Nordic Countries:", nordic_countries)  
+print("Estonia:", es)                         
+print("Russia:", ru)     
