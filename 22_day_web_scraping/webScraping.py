a4d5bc7cb53dc399efc88716a1c0aa779b934696
DaleLore <DaleLoreNY@gmail.com>
2024-09-22 21:39:31 -0500

    Day 22 Exercises

---
+++ 22_day_web_scraping/webScraping.py
@@ -0,0 +1,10 @@
+# <!-- Day 22: 30 Days of python programming -->
+
+# # Exercises - Day 22
+# - [beautifulsoup Documentation](https://www.crummy.com/software/BeautifulSoup/bs4/doc/#quick-start)
+
+# 1. Scrape the following website and store the data as json file(url = 'http://www.bu.edu/president/boston-university-facts-stats/').
+
+# 2. Extract the table in this url (https://archive.ics.uci.edu/ml/datasets.php) and change it to a json file
+
+# 3. Scrape the presidents table and store the data as json(https://en.wikipedia.org/wiki/List_of_presidents_of_the_United_States). The table is not very structured and the scrapping may take very long time.
